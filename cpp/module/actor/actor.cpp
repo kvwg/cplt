d9206056a42bb5db4f8efe27926e6d8568680c22
kvwg <wg911@126.com>
2017-10-08 21:02:20 +0800

    add init actor
    Signed-off-by: kvwg <wg911@126.com>

---
+++ cpp/module/actor/actor.cpp
@@ -0,0 +1,13 @@
+#include "actor.hpp"
+
+using namespace cplt;
+
+Actor::Actor()
+{
+
+}
+
+Actor::~Actor()
+{
+
+}
---
+++ cpp/module/actor/actor.hpp
@@ -0,0 +1,18 @@
+
+#ifndef CPLT_MODULE_ACTOR_HPP__
+#define CPLT_MODULE_ACTOR_HPP__
+
+namespace cplt {
+
+	class Actor
+	{
+	public:
+		Actor();
+		virtual ~Actor();
+	protected:
+		
+	private:
+	};
+
+}
+#endif // CPLT_MODULE_ACTOR_HPP__
